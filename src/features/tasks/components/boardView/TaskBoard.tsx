844c945de7a8956d86fe7518af6d1c4cc602aa42
HaidyHisham <Haidyhesham221@gmail.com>
2026-08-14 18:41:24 +0300

    feat: add TaskBoard column ui

--- src/features/tasks/components/boardView/TaskBoard.tsx
+++ src/features/tasks/components/boardView/TaskBoard.tsx
@@ -1,13 +1,27 @@
 
-import TaskHeader from '../TaskHeader'
+import TaskHeader from '../TaskHeader';
+import TaskBoardColumn from './TaskBoardColumn';
+import { TASK_STATUS } from '../../types/tasks.types';
+import { mockTasks } from '../../types/mockTasks';
+
+const statusList = Object.values(TASK_STATUS);
 
 function TaskBoard() {
     return (
         <section className="flex flex-col gap-6">
             <TaskHeader />
+          <div className="hidden lg:flex gap-6 w-full pb-3 h-full flex-1 overflow-x-auto scroll">
+            {statusList.map((status) => (
+        <TaskBoardColumn
+            key={status}
+            status={status}
+            tasks={mockTasks}
             
+        />
+    ))}
+</div>
         </section>
-    )
+    );
 }
 
-export default TaskBoard
+export default TaskBoard;
